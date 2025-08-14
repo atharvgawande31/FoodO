a41d3f0d3aae75862dbdf8980ec3f861b859652f
atharvgawande31 <atharvdgawande@gmail.com>
2025-08-14 10:23:58 +0530

    rendering productList in FlatList an also impiment some container styling and Image properties

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,16 +3,20 @@ import Colors from '@/constants/Colors';
 import products from '@assets/data/products';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
-import {  ScrollView } from 'react-native'
+import {  View, FlatList } from 'react-native'
 import { ProductList } from '@/components/ProductList';
 
 
 export default function TabOneScreen() {
   return (
-    <ScrollView>
-      <ProductList product={products[1]} />
-      <ProductList product={products[2]} /> 
-      <ProductList product={products[3]} />
-    </ScrollView>
+     <View>
+        <FlatList 
+            data={products}
+            renderItem={({item}) => <ProductList product={item}/>}
+            numColumns={2}
+            contentContainerStyle={{gap: 10, padding: 10}}
+            columnWrapperStyle={{ gap: 10}}
+        />
+     </View>
   );
 }
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,42 +1,47 @@
-import { Product } from '@assets/types';
-import { View, Text } from './Themed';
-import { StyleSheet } from 'react-native';
-import { Image } from 'react-native';
-import Colors from '../constants/Colors';
-
-
-
+import { Product } from "@assets/types";
+import { View, Text } from "./Themed";
+import { StyleSheet } from "react-native";
+import { Image } from "react-native";
+import Colors from "../constants/Colors";
 
 export const ProductList = ({ product }: { product: Product }) => (
-  <View style={styles.container}>
-    <Image source={{ uri: product.image || 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png' }} style={styles.image} />
-    <Text style={styles.title}>{product.name}</Text>
-    <Text style={styles.price}>${product.price}</Text>
-  </View>
+    <View style={styles.container}>
+        <Image
+            source={{
+                uri:
+                    product.image ||
+                    "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png",
+
+            }}
+            resizeMode="contain"
+            style={styles.image}
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+    </View>
 );
 
-const styles = StyleSheet.create({ 
- 
-  container:{
-    flex:0,
-    margin:16,
-    padding:16,
-    backgroundColor:"white",
-    borderRadius: 20
-  },
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: "white",
+        borderRadius: 8,
+        maxWidth: "50%",
+    },
 
-  title: {
-    fontSize: 20,
-    fontWeight: 600,
-    marginVertical:8
-  },
-  image:{
-    width: "100%",
-    aspectRatio:1
-  },
-  price:{
-    fontSize:15,
-    fontWeight: 700,
-    color: Colors.light.tint
-  }
+    title: {
+        fontSize: 20,
+        fontWeight: 600,
+        marginVertical: 8,
+    },
+    image: {
+        width: "100%",
+        aspectRatio: 1,
+    },
+    price: {
+        fontSize: 15,
+        fontWeight: 700,
+        color: Colors.light.tint,
+    },
 });
